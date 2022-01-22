143f93936fb394601912cfdbe6cdcf84b1e973cd
smiling <2766994545@qq.com>
2022-01-22 11:09:49 +0800

    添加icons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as Icons from '@element-plus/icons'
+import { toLine } from './utils'
 
 const app = createApp(App)
+
+// // 全局注册图标 牺牲一点性能
+// // el-icon-xxx
+for (let i in Icons) {
+  // console.log(i)
+  // 注册全部组件
+  app.component(`el-icon-${toLine(i)}`, (Icons as any)[i])
+}
 app.use(router).use(ElementPlus).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>首页</h1>
   <el-button>aaa</el-button>
+  <el-icon-timer></el-icon-timer>
 </template>
 
 <script lang="ts">
