df4aba383e39fcb9aa978d803346c965338a26cc
smiling <2766994545@qq.com>
2022-01-22 10:04:06 +0800

    feat 引入element

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-app.use(router).mount('#app')
+app.use(router).use(ElementPlus).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>首页</h1>
+  <el-button>aaa</el-button>
 </template>
 
 <script lang="ts">
