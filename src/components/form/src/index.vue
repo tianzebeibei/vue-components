70b71642b2242a318cf7cfe7e9a5cd957a04bbf0
smiling <2766994545@qq.com>
2022-01-27 13:47:42 +0800

    feat form功能

--- src/components/form/src/index.vue
+++ src/components/form/src/index.vue
@@ -15,9 +15,27 @@
         <component
           v-model="modal[item.prop!]"
           v-bind="item.attrs"
+          v-if="item.type !== 'upload'"
           :is="`el-${item.type}`"
           :placeholder="item.placeholder"
         ></component>
+        <el-upload
+          v-bind="item.uploadAttrs"
+          v-else
+          :on-preview="onPreview"
+          :on-remove="onRemove"
+          :on-success="onSuccess"
+          :on-error="onError"
+          :on-progress="onProgress"
+          :on-change="onChange"
+          :before-upload="beforeUpload"
+          :before-remove="beforeRemove"
+          :http-request="httpRequest"
+          :on-exceed="onExceed"
+        >
+          <slot name="uploadArea"></slot>
+          <slot name="uploadTip"></slot>
+        </el-upload>
       </el-form-item>
       <!-- 类似select这种有select的情况 -->
       <el-form-item
@@ -55,7 +73,23 @@ let props = defineProps({
     type: Array as PropType<FormOptions[]>,
     require: true,
   },
+  // 用户自定义上传方法
+  httpRequest: {
+    type: Function
+  }
 });
+let emits = defineEmits([
+  "on-preview",
+  "on-remove",
+  "on-success",
+  "on-error",
+  "on-progress",
+  "on-change",
+  "before-upload",
+  "before-remove",
+  'http-request',
+  'on-exceed'
+]);
 //  {username: '', password: ''}
 let modal = ref<any>(null);
 let rules = ref<any>(null);
@@ -73,6 +107,37 @@ let initForm = () => {
   }
   // console.log(modal.value,rules.value)
 };
+// 上传组件的所有方法
+let onPreview = (file: File) => {
+  emits('on-preview', file)
+}
+let onRemove = (file: File, fileList: FileList) => {
+  emits('on-remove', { file, fileList })
+}
+let onSuccess = (response: any, file: File, fileList: FileList) => {
+  // 上传图片成功 给表单上传项赋值
+  // let uploadItem = props.options.find(item => item.type === 'upload')!
+  // model.value[uploadItem.prop!] = { response, file, fileList }
+  emits('on-success', { response, file, fileList })
+}
+let onError = (err: any, file: File, fileList: FileList) => {
+  emits('on-error', { err, file, fileList, })
+}
+let onProgress = (event: any, file: File, fileList: FileList) => {
+  emits('on-progress', { event, file, fileList })
+}
+let onChange = (file: File, fileList: FileList) => {
+  emits('on-change', { file, fileList })
+}
+let beforeUpload = (file: File) => {
+  emits('before-upload', file)
+}
+let beforeRemove = (file: File, fileList: FileList) => {
+  emits('before-remove', { file, fileList })
+}
+let onExceed = (files: File, fileList: FileList) => {
+  emits('on-exceed', { files, fileList })
+}
 onMounted(() => {
   initForm();
 });
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <m-form :options="options" label-width="200px"></m-form>
+  <div>
+    <m-form
+      :options="options"
+      label-width="200px"
+      @on-preview="handlePreview"
+      @on-remove="handleRemove"
+      @before-remove="beforeRemove"
+      @on-success="handleSuccesee"
+      @on-exceed="handleExceed"
+      @on-error="handleError"
+      multiple
+    >
+      <template #uploadArea>
+        <el-button class="ml-3" type="success"
+          >upload to server</el-button
+        ></template
+      >
+      <template #uploadTip>
+        <div style="color: #ccc">
+          limit 1 file, new file will cover the old file
+        </div>
+      </template>
+    </m-form>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { FormOptions } from "../../components/form/src/types/types";
-
+import type { UploadFile } from "element-plus/es/components/upload/src/upload.type";
+import { ElMessageBox } from "element-plus";
 let options: FormOptions[] = [
   {
     type: "input",
@@ -56,7 +80,7 @@ let options: FormOptions[] = [
   {
     type: "select",
     value: "2",
-    label:'职位',
+    label: "职位",
     placeholder: "请选择职位",
     prop: "role",
     rules: [
@@ -71,54 +95,98 @@ let options: FormOptions[] = [
       { type: "option", label: "主管", value: "2" },
       { type: "option", label: "员工", value: "3" },
     ],
-    attrs:{
-      style:{
-        width:'500px'
-      }
-    }
+    attrs: {
+      style: {
+        width: "500px",
+      },
+    },
   },
   {
-    type:'checkbox-group',
-    value:[],
-    prop:'like',
-    label:'爱好',
-    children:[
+    type: "checkbox-group",
+    value: [],
+    prop: "like",
+    label: "爱好",
+    children: [
       {
-        type:'checkbox',
-        label:'足球',
-        value:'1'
-      },{
-        type:'checkbox',
-        label:'篮球',
-        value:'2'
-      },{
-        type:'checkbox',
-        label:'网球',
-        value:'3'
-      }
-    ]
-  },{
-    type:'radio-group',
-    value:[],
-    prop:'like',
-    label:'爱好',
-    children:[
+        type: "checkbox",
+        label: "足球",
+        value: "1",
+      },
       {
-        type:'radio',
-        label:'足球',
-        value:'1'
-      },{
-        type:'radio',
-        label:'篮球',
-        value:'2'
-      },{
-        type:'radio',
-        label:'网球',
-        value:'3'
-      }
-    ]
-  }
+        type: "checkbox",
+        label: "篮球",
+        value: "2",
+      },
+      {
+        type: "checkbox",
+        label: "网球",
+        value: "3",
+      },
+    ],
+  },
+  {
+    type: "radio-group",
+    value: "",
+    prop: "gender",
+    label: "性别",
+    rules: [
+      {
+        required: true,
+        message: "性别不能为空",
+        trigger: "change",
+      },
+    ],
+    children: [
+      {
+        type: "radio",
+        label: "男",
+        value: "male",
+      },
+      {
+        type: "radio",
+        label: "女",
+        value: "female",
+      },
+      {
+        type: "radio",
+        label: "保密",
+        value: "not",
+      },
+    ],
+  },
+  {
+    type: "upload",
+    label: "上传",
+    prop: "pic",
+    rules: [
+      {
+        required: true,
+        message: "不能为空",
+        trigger: "blur",
+      },
+    ],
+    uploadAttrs: {
+      action: "https://jsonplaceholder.typicode.com/posts/",
+    },
+  },
 ];
+
+const handleRemove = (file: UploadFile, fileList: UploadFile[]) => {
+  console.log(file, fileList);
+};
+const handlePreview = (file: UploadFile) => {
+  console.log(file);
+};
+const beforeRemove = (file: UploadFile, fileList: UploadFile[]) => {
+  return ElMessageBox.confirm(`Cancel the transfert of ${file.name} ?`);
+};
+const handleSuccesee = () => {
+  console.log("success");
+};
+const handleError = (val: any) => {
+  console.log("error", val);
+};
+const handleExceed = () => {};
 </script>
 
 <style lang="scss" scope></style>
